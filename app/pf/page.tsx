5e75fab22ce48e51d6b8e5570a17597fccfa81fe
김동혁 <132553964+ddhelop@users.noreply.github.com>
2025-04-11 16:21:20 +0900

    feat(pf): SSG 적용을 위한 다이나믹 선언 #1

--- app/pf/page.tsx
+++ app/pf/page.tsx
@@ -3,6 +3,8 @@ import IntroSection from './sections/IntroSection';
 import ProjectsSection from './sections/ProjectsSection';
 import SkillsSection from './sections/SkillsSection';
 
+export const dynamic = 'force-static';
+
 export default function PortFolioPage() {
   return (
     <main className="mx-auto max-w-3xl px-6 py-24 space-y-32">
