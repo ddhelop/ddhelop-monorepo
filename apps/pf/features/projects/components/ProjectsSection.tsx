64e80545f607bd3eb013eacaf48165c4e863cdb9
김동혁 <clfgnm9@naver.com>
2025-04-21 14:44:17 +0900

    chore: 프로젝트 순서 재배치

--- apps/pf/features/projects/components/ProjectsSection.tsx
+++ apps/pf/features/projects/components/ProjectsSection.tsx
@@ -9,17 +9,17 @@ export default function ProjectsSection() {
     <section id="projects" className="space-y-12">
       <h1 className="text-3xl font-bold">Projects</h1>
       <div className="space-y-20">
+        <div id="project-linkit">
+          <ProjectItem data={linkitData} />
+        </div>
+        <div id="project-portfolio">
+          <ProjectItem data={portfolioData} />
+        </div>
         <div id="project-moa">
           <ProjectItem data={moaData} />
         </div>
         <div id="project-ibt">
           <ProjectItem data={ibtData} />
-        </div>
-        <div id="project-portfolio">
-          <ProjectItem data={portfolioData} />
-        </div>
-        <div id="project-linkit">
-          <ProjectItem data={linkitData} />
         </div>
       </div>
     </section>
